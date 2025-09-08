e39fede75ed86cae61ec81574c279748277b1486
PriyajitMaity <priyajitmaity023@gmail.com>
2025-09-08 10:30:24 +0530

    add .github

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  base: "/Accuknox-dashboard/",
+  server: {
+    open: true,
+    server: 3001,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      
     },
   },
-})
+});
